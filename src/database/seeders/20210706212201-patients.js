302d3314bbfebc0febf66796903e7573f630ea3d
Michelly <michellynarita@hotmail.com>
2021-06-07 21:49:51 -0300

    Atualizando seeders

--- src/database/seeders/20210706212201-patients.js
+++ src/database/seeders/20210706212201-patients.js
@@ -23,6 +23,24 @@ module.exports = {
                     email: "joaquininharia@gmail.com",
                     phone: "(41)43438787"
                 },
+                {
+                    id: 4,
+                    name: "Fernando Henrique Cardoso",
+                    email: "fhcardoso@gmail.com",
+                    phone: "(41)12568753"
+                },
+                {
+                    id: 5,
+                    name: "Thamires Da Cunha",
+                    email: "thamiscunha@msn.com",
+                    phone: "(41)98987677"
+                },
+                {
+                    id: 6,
+                    name: "Matheus Fagundes",
+                    email: "fagundesmath@hotmail.com",
+                    phone: "(41)89998888"
+                },
             ],
         {}
         );
--- src/database/seeders/20210706222112-schedules.js
+++ src/database/seeders/20210706222112-schedules.js
@@ -26,6 +26,27 @@ module.exports = {
                     appointmentDate: "2021-07-08",
                     description: "A paciente se queixa de dores de cabeca frequentes"
                 },
+                {
+                    id: 4,
+                    doctorId: 1,
+                    patientId: 4,
+                    appointmentDate: "2021-07-10",
+                    description: "O paciente possui sequelas de AVC"
+                },
+                {
+                    id: 5,
+                    doctorId: 2,
+                    patientId: 5,
+                    appointmentDate: "2021-04-22",
+                    description: "A paciente se queixa de cólicas que náo param"
+                },
+                {
+                    id: 6,
+                    doctorId: 3,
+                    patientId: 6,
+                    appointmentDate: "2021-07-03",
+                    description: "O paciente se queixa de falta de sono"
+                },
             ],
         {}
         );
